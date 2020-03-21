7c35a4057394d99db07c9887f6f0b068aa0aeb06
vijit.sharma2222@yahoo.in <Vijit.Sharma2222@yahoo.in>
2020-03-21 20:14:23 +0530

    Updates

---
+++ precache-manifest.a3f72142f4e852d7e80fe71121c6e37c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c6352189d47c58808f08b35feae0ce9",
+    "url": "/portal/index.html"
+  },
+  {
+    "revision": "b5e9323f18398c1c894e",
+    "url": "/portal/static/css/2.6e9a8f64.chunk.css"
+  },
+  {
+    "revision": "f08f02af793628177e27",
+    "url": "/portal/static/css/main.e50231f6.chunk.css"
+  },
+  {
+    "revision": "b5e9323f18398c1c894e",
+    "url": "/portal/static/js/2.90d9f8ac.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/portal/static/js/2.90d9f8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f08f02af793628177e27",
+    "url": "/portal/static/js/main.b7787627.chunk.js"
+  },
+  {
+    "revision": "1421a0e2c121c71f9b09",
+    "url": "/portal/static/js/runtime-main.8778c6d7.js"
+  },
+  {
+    "revision": "00c4930a97a1b26b3f3f550330ad0986",
+    "url": "/portal/static/media/Aquaman.00c4930a.jpg"
+  },
+  {
+    "revision": "3bb131d047074bbd2e64b8a540874560",
+    "url": "/portal/static/media/GoodNewz.3bb131d0.jpg"
+  },
+  {
+    "revision": "66609957170b2a304bbce81e13f40c1f",
+    "url": "/portal/static/media/Hunters.66609957.jpg"
+  },
+  {
+    "revision": "a54bfc4d2ee07c335a7541cd21617044",
+    "url": "/portal/static/media/StreetDancer.a54bfc4d.jpg"
+  },
+  {
+    "revision": "726807e0968a964a596c4ded05547086",
+    "url": "/portal/static/media/Test.726807e0.jpg"
+  },
+  {
+    "revision": "666eda81fbe4319f30c39c7b90829fa3",
+    "url": "/portal/static/media/healthcare.666eda81.gif"
+  },
+  {
+    "revision": "8bea7b289d8c7f4f6d7bf08c43be58d3",
+    "url": "/portal/static/media/sad.8bea7b28.gif"
+  },
+  {
+    "revision": "ea7624defee2131651b110ac2286d58d",
+    "url": "/portal/static/media/success.ea7624de.gif"
+  }
+]);
