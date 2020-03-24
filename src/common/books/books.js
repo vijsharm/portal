31eb7fba196aab329b1f2bf2f5de92f69f4de2bf
Roohani Singla <roohani.singla@trantorinc.com>
2020-03-24 15:50:23 +0530

    change

--- src/common/books/books.js
+++ src/common/books/books.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import BookImg from '../../assets/images/books/book.jpg'
+import BookImg from '../../assets/images/books/book.jpg';
+import './books.scss'
 
 const Books = (props)=> {
     const titlesList = props.data.map((item, index) => {
     return <div>
         <div className="row">
-            <div className="col-md-1"><img src={item.img} height="100" width="60"></img></div>
-            <div className="col-md-11">
+            <div className="col-md-1 col-xs-2"><img src={item.img} height="100" width="60"></img></div>
+            <div className="col-md-11 col-xs-10">
             <span style={{ fontSize: "18px", fontWeight: 'bold', textDecoration:'underline', paddingLeft : '15px' }}>{item.title}</span>
             <div>{item.description}</div>
             </div>
@@ -19,7 +20,7 @@ const Books = (props)=> {
         <div className="books">
             <br/>
             <div className="heading">
-                <img src={BookImg}></img> <span> ~ “A reader lives a thousand lives before he dies . . . The man who never reads lives only one.” – George R.R. Martin</span>
+                <img src={BookImg}></img> <span className="quote"> ~ “A reader lives a thousand lives before he dies . . . The man who never reads lives only one.” – George R.R. Martin</span>
             </div>
             <hr/>
             <div className="bodySection col-sm-12">
